f03a1936e5ff5dcfa5755217ae57c2303d9b855d
mark <markdamonrood@gmail.com>
2018-09-17 14:12:46 -0500

    Initial Multmodule forgot main in web module

---
+++ pet-clinic-web/src/main/java/rood/mark/sfgnpetclinic/SfgnPetClinicApplication.java
@@ -0,0 +1,12 @@
+package rood.mark.sfgnpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgnPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgnPetClinicApplication.class, args);
+    }
+}
