4f54e776fd11c1195fb06092b1104282da1f3a54
mark <markdamonrood@gmail.com>
2018-09-18 08:54:18 -0500

    implement interfaces for services

---
+++ pet-clinic-data/src/main/java/rood/mark/sfgnpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package rood.mark.sfgnpetclinic.services;
+
+import rood.mark.sfgnpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/rood/mark/sfgnpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package rood.mark.sfgnpetclinic.services;
+
+import rood.mark.sfgnpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
