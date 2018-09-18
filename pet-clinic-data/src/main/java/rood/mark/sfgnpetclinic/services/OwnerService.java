0041d62b5dc8f9c9d2a6a6e629418990fc3f834e
mark <markdamonrood@gmail.com>
2018-09-18 09:27:26 -0500

    refactor service interfaces

--- pet-clinic-data/src/main/java/rood/mark/sfgnpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/rood/mark/sfgnpetclinic/services/OwnerService.java
@@ -2,12 +2,8 @@ package rood.mark.sfgnpetclinic.services;
 
 import rood.mark.sfgnpetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner,Long>{
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/rood/mark/sfgnpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/rood/mark/sfgnpetclinic/services/VetService.java
@@ -2,10 +2,6 @@ package rood.mark.sfgnpetclinic.services;
 
 import rood.mark.sfgnpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
