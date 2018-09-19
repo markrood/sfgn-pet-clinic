2cd2bad3fa1081bd06ad21448f927c127a02df12
mark <markdamonrood@gmail.com>
2018-09-19 14:13:54 -0500

    remove id setting on save

--- pet-clinic-data/src/main/java/rood/mark/sfgnpetclinic/services/map/VetSeviceMap.java
+++ pet-clinic-data/src/main/java/rood/mark/sfgnpetclinic/services/map/VetSeviceMap.java
@@ -21,7 +21,7 @@ public class VetSeviceMap extends AbstractMapService<Vet,Long> implements VetSer
 
     @Override
     public Vet save(Vet object) {
-        return super.save(object.getId(),object);
+        return super.save(object);
     }
 
     @Override
--- pet-clinic-web/src/main/java/rood/mark/sfgnpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/rood/mark/sfgnpetclinic/bootstrap/DataLoader.java
@@ -24,14 +24,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Dick");
         owner1.setLastName("Moe");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Skippy");
         owner2.setLastName("DaBush");
 
@@ -40,14 +38,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Joe");
         vet1.setLastName("Surf");
 
         vetSerice.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("My");
         vet2.setLastName("Money");
 
