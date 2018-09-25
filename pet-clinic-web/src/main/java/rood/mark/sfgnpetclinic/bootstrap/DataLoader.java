478cc5be765238bfd7ee176a1339883edcd31357
mark <markdamonrood@gmail.com>
2018-09-25 10:11:56 -0500

    added pet type in bootstrap data loader

--- pet-clinic-web/src/main/java/rood/mark/sfgnpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/rood/mark/sfgnpetclinic/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package rood.mark.sfgnpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import rood.mark.sfgnpetclinic.model.Owner;
+import rood.mark.sfgnpetclinic.model.PetType;
 import rood.mark.sfgnpetclinic.model.Vet;
 import rood.mark.sfgnpetclinic.services.OwnerService;
+import rood.mark.sfgnpetclinic.services.PetTypeService;
 import rood.mark.sfgnpetclinic.services.VetService;
 
 @Component
@@ -12,16 +14,28 @@ public class DataLoader implements CommandLineRunner {
 
     private final VetService vetSerice;
     private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(VetService vetSerice, OwnerService ownerService) {
+    public DataLoader(VetService vetSerice, OwnerService ownerService, PetTypeService petTypeService) {
         this.vetSerice = vetSerice;
         this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
     }
 
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType saveDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType saveCatPetType = petTypeService.save(cat);
+
+        System.out.println("Loaded Pet Type");
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Dick");
