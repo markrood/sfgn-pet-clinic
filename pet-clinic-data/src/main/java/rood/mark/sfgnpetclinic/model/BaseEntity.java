3de69f9f6cca6b2913f2f82b4989b218feaea848
mark <markdamonrood@gmail.com>
2018-10-04 10:26:10 -0500

    jpa for base entity

--- pet-clinic-data/src/main/java/rood/mark/sfgnpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/rood/mark/sfgnpetclinic/model/BaseEntity.java
@@ -1,8 +1,14 @@
 package rood.mark.sfgnpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
